09ce61f7c012311471fa661f061fd50be2221c09
kadonomaro <denis.afer@gmail.com>
2020-04-10 20:48:06 +0700

    fix: some canvas, hero change

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const heroTitle = document.querySelector('.hero__title');
     const heroSubtitle = document.querySelector('.hero__subtitle');
 
-
     heroTitle.classList.add('hero__title--fade-in');
     heroSubtitle.classList.add('hero__subtitle--fade-in');
 
@@ -18,17 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const width = canvas.width;
         const height = canvas.height;
 
-        ctx.font = "bold 16px Arial";
+        ctx.font = "bold 16px Roboto";
 
         render(20,20,10);
 
 
         function render(cellWidth = 20, cellHeight = 20, speed = 100) {
             const renderCount = (width / cellWidth) * (height / cellHeight);
+            const renderLimit = renderCount - renderCount * 0.5;
             let counter = 0;
             const positions = [];
-            // const colors = ['#3acd3d','#3bb43e', '#349b37', '#38823b'];
-            const colors = ['#ffffff','#e6e6e6', '#cdcdcd', '#b4b4b4'];
+            const colors = ['#e6e6e6','#cdcdcd', '#b4b4b4', '#9b9b9b'];
 
             const interval = setInterval(() => {
                 const position = {
@@ -40,13 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     ctx.fillStyle = colors[getRandomRange(0, colors.length - 1)];
                     ctx.fillText(getRandomSymbol(65, 122), position.x, position.y);
                     counter++;
-                } else {
-                    counter--;
                 }
                 positions.push(position);
 
-
-                if (counter >= renderCount) {
+                if (counter >= renderLimit) {
                     clearInterval(interval);
                 }
             }, speed);

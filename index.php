c7f3f3707b53e58196ea5d5eb6c738bb7ec0045b
kadonomaro <denis.afer@gmail.com>
2020-04-11 20:44:32 +0700

    feat: add page footer

--- index.php
+++ index.php
@@ -440,6 +440,11 @@
 
             <footer class="products__footer">
                 <span class="products__resume">Выбор всегда остается за вами</span>
+                <div class="products__order">
+                    <form action="" class="order">
+                        <button class="order__button button">Присоединиться</button>
+                    </form>
+                </div>
             </footer>
         </div><!--products--inner-->
     </section>
@@ -543,6 +548,37 @@
 
         </div><!-- /.container -->
     </section>
+
+
+    <footer class="page-footer">
+        <div class="page-footer__top">
+            <span class="page-footer__text">Мы всегда готовы к сотрудничеству</span>
+            <a href="tel:88006001212" class="page-footer__phone">8-800-600-12-12</a>
+        </div>
+        <div class="page-footer__bottom">
+            <span class="page-footer__year">© 1998 - <?php echo date('Y')?></span>
+            <div class="page-footer__nav">
+                <nav class="footer-nav">
+                    <ul class="footer-nav__list">
+                        <li class="footer-nav__item">
+                            <a href="/" class="footer-nav__link">Главная</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a href="/products" class="footer-nav__link">Продукты</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="page-footer__logo">
+                <div class="logo">
+                    <div class="logo__image">
+                        <img src="dist/assets/image/logo.svg" alt="Airsoftware">
+                    </div>
+                    <span class="logo__text"></span>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>

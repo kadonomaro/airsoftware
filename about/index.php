a2fed834454ab658d9daba7ca891102182bf6d19
kadonomaro <denis.afer@gmail.com>
2020-05-07 21:37:51 +0700

    feat: add company page start layout

---
+++ about/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once '../partials/favicon.php'?>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/dist/css/style.css">
+    <title>О компании - Airsoftware</title>
+    <meta name="description" content="Компания Airsoftware - это надежный партнер в решении бизнес задач. Информация о компании Airsoftware">
+</head>
+<body class="company-page">
+
+    <main class="company-hero">
+        <div class="company-hero__header">
+            <?php include_once '../partials/page-header.php'?>
+        </div>
+    </main>
+
+
+    <section class="company-map section">
+
+    </section>
+
+
+    <?php include_once '../partials/page-footer.php'?>
+
+
+    <div class="modal-overlay">
+        <?php include_once '../partials/order-modal.php'?>
+    </div>
+
+
+    <script src="../dist/js/bundle.js"></script>
+</body>
+</html>

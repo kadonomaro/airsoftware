1afb149c1e01b5e5d36d89bc72de7db35a81168d
kadonomaro <denis.afer@gmail.com>
2020-08-09 13:52:22 +0700

    feat: add team php template for index page

--- index.php
+++ index.php
@@ -452,6 +452,7 @@
 
 
     <section class="team section">
+        <?php include_once 'php/data/team.php'?>
         <div class="container">
             <header class="section__header">
                 <h2 class="section__title">Наша команда</h2>
@@ -459,92 +460,30 @@
             </header>
 
             <div class="team__inner">
-                <div class="team__card">
-                    <h3 class="team__title">Основатели</h3>
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/oleg-komarov.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/oleg-komarov.jpg" alt="Олег Комаров" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Олег Комаров</span>
-                            <span class="team-card__role">Основатель, президент</span>
-                        </div>
-                    </article>
 
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/david-denisov.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/david-denisov.jpg" alt="Давид Денисов" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Давид Денисов</span>
-                            <span class="team-card__role">Основатель, президент</span>
-                        </div>
-                    </article>
-                </div><!-- /.team__card -->
+                <? foreach ($team as $department): ?>
+                    <div class="team__card">
+                        <h3 class="team__title"><?=$department["title"]?></h3>
 
-                <div class="team__card">
-                    <h3 class="team__title">Директора</h3>
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/joe-noname.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/joe-noname.jpg" alt="Джо Ноунейм" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Джо Ноунейм</span>
-                            <span class="team-card__role">Генеральный директор</span>
-                        </div>
-                    </article>
+                        <? foreach ($department as $member): ?>
+                            <? if(gettype($member) == 'array'): ?>
+                                <article class="team-card">
+                                    <div class="team-card__image">
+                                        <picture>
+                                            <source type="image/webp" srcset="<?=$member["src"]?>.webp">
+                                            <img class="team-card__image-img" src="<?=$member["src"]?>.jpg" alt="<?=$member["name"]?>" >
+                                        </picture>
+                                    </div>
+                                    <div class="team-card__text">
+                                        <span class="team-card__name"><?=$member["name"]?></span>
+                                        <span class="team-card__role"><?=$member["post"]?></span>
+                                    </div>
+                                </article>
+                            <? endif; ?>
+                        <? endforeach; ?>
+                    </div><!-- /.team__card -->
+                <? endforeach; ?>
 
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/anna-orehova.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/anna-orehova.jpg" alt="Анна Орехова" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Анна Орехова</span>
-                            <span class="team-card__role">Финансовый директор</span>
-                        </div>
-                    </article>
-                </div><!-- /.team__card -->
-
-                <div class="team__card">
-                    <h3 class="team__title">Разработчики</h3>
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/maxim-mohov.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/maxim-mohov.jpg" alt="Максим Мохов" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Максим Мохов</span>
-                            <span class="team-card__role">Руководитель департамента разработки</span>
-                        </div>
-                    </article>
-
-                    <article class="team-card">
-                        <div class="team-card__image">
-                            <picture>
-                                <source type="image/webp" srcset="dist/assets/image/team/anton-ivanov.webp">
-                                <img class="team-card__image-img" src="dist/assets/image/team/anton-ivanov.jpg" alt="Антон Иванов" >
-                            </picture>
-                        </div>
-                        <div class="team-card__text">
-                            <span class="team-card__name">Антон Иванов</span>
-                            <span class="team-card__role">Ведущий инженер</span>
-                        </div>
-                    </article>
-                </div><!-- /.team__card -->
             </div><!-- /.team__inner -->
 
         </div><!-- /.container -->
---
+++ php/data/team.php
@@ -0,0 +1,44 @@
+<?
+$team = array(
+    array(
+        "title" => "Основатели",
+        array(
+            "name" => "Олег Комаров",
+            "post" => "Основатель, президент",
+            "src" => "dist/assets/image/team/oleg-komarov"
+        ),
+        array(
+            "name" => "Давид Денисов",
+            "post" => "Основатель, президент",
+            "src" => "dist/assets/image/team/david-denisov"
+        )
+    ),
+
+    array(
+        "title" => "Директора",
+        array(
+            "name" => "Джо Ноунейм",
+            "post" => "Генеральный директор",
+            "src" => "dist/assets/image/team/joe-noname"
+        ),
+        array(
+            "name" => "Анна Орехова",
+            "post" => "Финансовый директор",
+            "src" => "dist/assets/image/team/anna-orehova"
+        )
+    ),
+
+    array(
+        "title" => "Разработчики",
+        array(
+            "name" => "Максим Мохов",
+            "post" => "Руководитель департамента разработки",
+            "src" => "dist/assets/image/team/maxim-mohov"
+        ),
+        array(
+            "name" => "Антон Иванов",
+            "post" => "Ведущий инженер",
+            "src" => "dist/assets/image/team/anton-ivanov"
+        )
+    ),
+);

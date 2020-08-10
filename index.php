debc78bdd6e9cd23873ebb2b7082b18081ca2582
kadonomaro <denis.afer@gmail.com>
2020-08-10 17:06:23 +0700

    feat: update blog section style/template

--- index.php
+++ index.php
@@ -452,14 +452,11 @@
 
 
     <section class="blog">
-            <div class="blog__inner">
-                <div class="blog__main">
-                    <h2 class="blog__title">Index Word</h2>
-                    <span class="blog__subtitle">Очередной ИТ блог обо всем</span>
-                    <a class="button blog__link" href="https://index-word.web.app/" rel="noopener noreferrer" target="_blank">Читать</a>
-                </div>
-                <div class="blog__side"></div>
-            </div><!-- ./blog__inner -->
+        <div class="blog__text">
+            <h2 class="blog__title">Index Word</h2>
+            <span class="blog__subtitle">Очередной ИТ блог обо всем</span>
+            <a class="button button--small blog__link" href="https://index-word.web.app/" rel="noopener noreferrer" target="_blank">Читать</a>
+        </div>
     </section><!-- ./blog -->
 
 
